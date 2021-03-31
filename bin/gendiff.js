36d1e556095358c68f5b3d6ab3c6eaf73cc3bf63
ussury <meshalkins@ya.ru>
2021-03-31 11:32:15 +0300

    создал commander gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,9 @@
 #!/usr/bin/env node
+/* eslint-disable */
+import commander from 'commander';
+
+commander
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.1.0');
+
+commander.parse(process.argv);
