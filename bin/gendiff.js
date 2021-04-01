2a4b3d84425a58f712c4e82c0655067f04f40468
ussury <meshalkins@ya.ru>
2021-04-01 22:52:58 +0300

    gendiff refactor

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,7 @@ import program from 'commander';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format');
+
+program
   .parse(process.argv);
