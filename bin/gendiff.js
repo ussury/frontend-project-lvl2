65e2c6ab151d1cf1163d246c47c5aa99752dbe45
ussury <meshalkins@ya.ru>
2023-01-14 19:35:07 +0300

    опубликовал пакет в системе

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,8 +3,8 @@ import { Command } from "commander";
 
 const program = new Command();
 program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0');
+  .name("gendiff")
+  .description("Compares two configuration files and shows a difference.")
+  .version("0.1.0");
 
 program.parse();
