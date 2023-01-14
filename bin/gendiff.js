97b8e770ef81d5817bacb297ce7c3bc922b80508
ussury <meshalkins@ya.ru>
2023-01-14 19:48:51 +0300

    настройка аргументов  опций

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,9 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0');
+  .version('0.1.0')
+  .argument('<filepach1>')
+  .argument('<filepach2>')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
