c3ec6079007370b359ce27eafd28ea65af73551e
ussury <meshalkins@ya.ru>
2023-01-17 14:37:37 +0300

    добавил вывод ф-ии gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import { gendiff } from '../src/index.js';
 
 const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0')
-  .argument('<filepach1>')
-  .argument('<filepach2>')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(gendiff(filepath1, filepath2));
+  });
 
 program.parse();
