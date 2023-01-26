73a9f43121caa77ce7474140cd59e7b692091ff4
ussury <meshalkins@ya.ru>
2023-01-26 13:41:39 +0300

    добавил опцию стиля по умолчанию

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,11 +7,11 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(gendiff(filepath1, filepath2));
+    console.log(gendiff(filepath1, filepath2, program.opts().format));
   });
 
 program.parse();
