2bb9de5859f73f82ea6ffdc48c112df679b4de80
ussury <meshalkins@ya.ru>
2023-03-20 16:37:55 +0300

    refactor formats

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -3,6 +3,6 @@ import _ from 'lodash';
 import plain from './plain.js';
 import stylish from './stylish.js';
 
-const formats = { plain, stylish, json: JSON.stringify };
+const formats = { plain: plain, stylish: stylish, json: JSON.stringify };
 
 export default (style, tree) => formats[style](tree);
