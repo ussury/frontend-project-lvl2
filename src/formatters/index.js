f7a72a2282b6bf0e6eeef48cc88149f9abc38949
ussury <meshalkins@ya.ru>
2023-03-20 18:37:28 +0300

    new funk

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,6 +1,18 @@
 import plain from './plain.js';
 import stylish from './stylish.js';
 
-const formats = { plain, stylish, json: JSON.stringify };
+// const formats = { plain, stylish, json: JSON.stringify };
+// export default (style, tree) => formats[style](tree);
 
-export default (style, tree) => formats[style](tree);
+export default (style, tree) => {
+  switch (style) {
+    case 'stylish':
+      return stylish(tree);
+    case 'plain':
+      return plain(tree);
+    case 'json':
+      return JSON.stringify(tree);
+    default:
+      return `Something went wrong with ${style} format.`;
+  }
+};
